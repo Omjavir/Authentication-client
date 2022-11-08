075180ef06ed2b5d825b9caf1619962b416f2bea
Om Suresh Javir <javirom07042003@gmail.com>
2022-11-08 20:53:30 +0530

    added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -5,7 +5,7 @@ const Login = () => {
     const [password, setPassword] = useState('')
     async function loginUser(e) {
         e.preventDefault();
-        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/auth/login`, {
+        const response = await fetch(`http://localhost:5000/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -6,7 +6,7 @@ const Register = () => {
     const [password, setPassword] = useState('')
     async function registerUser(e) {
         e.preventDefault();
-        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/auth/register`, {
+        const response = await fetch(`http://localhost:5000/auth/register`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
